1c82936ef580af25150229d68536ccf482b230b1
Martin Harvan <martinhrvn@gmail.com>
2025-02-24 23:10:57 +0100

    Adding validations package

---
+++ packages/validations/src/hive/create-hive.ts
@@ -0,0 +1,9 @@
+import { HiveStatus } from "database";
+import {z} from "zod";
+
+export const createHiveSchema = z.object({
+    id: z.string().uuid(),
+    name: z.string(),
+    apiaryId: z.string().uuid().nullish(),
+    status: z.nativeEnum(HiveStatus)
+})
