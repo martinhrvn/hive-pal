8f4fa5cf1b7da75c8e92a15bb8395474ba1d093e
Martin Harvan <martinhrvn@gmail.com>
2025-03-05 08:48:42 +0100

    Refactor login page to use Label component and add dark mode support in CSS

--- apps/frontend/src/pages/login-page.tsx
+++ apps/frontend/src/pages/login-page.tsx
@@ -1,9 +1,9 @@
 import { FormEvent, useState } from "react";
 import { useAuth } from "@/context/auth-context.tsx";
 import { useNavigate } from "react-router-dom";
-import { FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
@@ -34,25 +34,18 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen  flex flex-col justify-center">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h1 className="text-center text-2xl font-semibold text-gray-900 mb-2">
+        <h1 className="text-center text-2xl font-semibold  mb-2">
           Beekeeping Manager
         </h1>
-        <h2 className="text-center text-lg text-gray-600 mb-8">
-          Sign in to your account
-        </h2>
+        <h2 className="text-center text-lg mb-8">Sign in to your account</h2>
       </div>
 
       {error && <p className={"color-red-400"}>{error}</p>}
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <div className="py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
-              <FormLabel
-                htmlFor="username"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Username
-              </FormLabel>
+              <Label htmlFor="username">Username</Label>
               <div className="mt-1">
                 <Input
                   id="username"
@@ -61,18 +54,12 @@ const LoginPage = () => {
                   required
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 />
               </div>
             </div>
 
             <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
+              <Label htmlFor="password">Password</Label>
               <div className="mt-1">
                 <Input
                   id="password"
@@ -81,18 +68,12 @@ const LoginPage = () => {
                   required
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 />
               </div>
             </div>
 
             <div>
-              <Button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                Sign in
-              </Button>
+              <Button type="submit">Sign in</Button>
             </div>
           </form>
         </div>
