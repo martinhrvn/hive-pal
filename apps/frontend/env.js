439cbf7da3d4d177c727aea1211eeb15a3e517dc
Martin Harvan <martinhrvn@gmail.com>
2025-03-05 09:12:53 +0100

    Improve environment variable handling in client.ts and env.js

--- apps/frontend/env.js
+++ apps/frontend/env.js
@@ -1,5 +1,3 @@
 window.ENV = {
-  VITE_API_URL: "$VITE_API_URL".startsWith("$")
-    ? import.meta.env.VITE_API_URL
-    : "$VITE_API_URL",
+  VITE_API_URL: "$VITE_API_URL",
 };
--- apps/frontend/src/api/client.ts
+++ apps/frontend/src/api/client.ts
@@ -1,5 +1,9 @@
 export const getEnvVariable = (key: string): string => {
-  if (window.ENV && window.ENV[key] !== undefined) {
+  if (
+    window.ENV &&
+    window.ENV[key] !== undefined &&
+    !window.ENV[key].startsWith("$")
+  ) {
     console.log("Getting env variable from window", key, window.ENV[key]);
     return window.ENV[key];
   }
