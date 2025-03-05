fe7fa33ad0dabd58447e1735efb85d3ea85b703f
Martin Harvan <martinhrvn@gmail.com>
2025-03-05 09:40:42 +0100

    Set global prefix to 'api' in main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     origin: allowedOrigins,
     credentials: true,
   });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Strip properties not in the DTO
