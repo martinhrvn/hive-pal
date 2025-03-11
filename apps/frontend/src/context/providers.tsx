06f8fac1f28b908227ef7aa5a59589f815bffbbf
Martin Harvan <martinhrvn@gmail.com>
2025-03-11 19:53:22 +0100

    Add ApiarySwitcher component to manage team selection in the sidebar

--- apps/frontend/src/context/providers.tsx
+++ apps/frontend/src/context/providers.tsx
@@ -4,15 +4,18 @@ import { ThemeProvider } from './theme-provider';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { getEnvVariable } from '@/api/client.ts';
+import { SidebarProvider } from '@/components/ui/sidebar.tsx';
 
 const queryClient = new QueryClient();
 axios.defaults.baseURL = getEnvVariable('VITE_API_URL');
 export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <AuthProvider>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider defaultTheme="light">{children}</ThemeProvider>
-      </QueryClientProvider>
-    </AuthProvider>
+    <SidebarProvider>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider defaultTheme="light">{children}</ThemeProvider>
+        </QueryClientProvider>
+      </AuthProvider>
+    </SidebarProvider>
   );
 };
