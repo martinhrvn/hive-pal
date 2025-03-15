ce2980aac6879a9a416ec11c8ecb5c3ba5d7de35
Martin Harvan <martinhrvn@gmail.com>
2025-03-16 00:21:11 +0100

    Refactor Providers component to conditionally render SidebarProvider: skip sidebar on specific pages for improved user experience

--- apps/frontend/src/context/providers.tsx
+++ apps/frontend/src/context/providers.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react';
+import { Fragment, PropsWithChildren } from 'react';
 import { AuthProvider } from '@/context/auth-context';
 import { ThemeProvider } from './theme-provider';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
@@ -8,14 +8,19 @@ import { SidebarProvider } from '@/components/ui/sidebar.tsx';
 
 const queryClient = new QueryClient();
 axios.defaults.baseURL = getEnvVariable('VITE_API_URL');
+
+const SKIP_SIDEBAR_PAGES = ['/login', '/register', '/onboarding'];
 export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
+  const pathname = window.location.pathname;
+  const skipSidebar = SKIP_SIDEBAR_PAGES.includes(pathname);
+  const SidebarProviderComponent = skipSidebar ? Fragment : SidebarProvider;
   return (
-    <SidebarProvider>
+    <SidebarProviderComponent>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <ThemeProvider defaultTheme="light">{children}</ThemeProvider>
         </AuthProvider>
       </QueryClientProvider>
-    </SidebarProvider>
+    </SidebarProviderComponent>
   );
 };
