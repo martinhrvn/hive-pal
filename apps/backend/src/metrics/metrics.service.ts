ceb50c2135a778a352da7002d7c8e0ce4b46378b
Martin Harvan <martinhrvn@gmail.com>
2025-03-29 14:10:06 +0100

    Refactor metrics service to import ObservationSchemaType from the shared-schemas package

--- apps/backend/src/metrics/metrics.service.ts
+++ apps/backend/src/metrics/metrics.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { ObservationSchemaType } from 'shared-schemas/dist';
+import { ObservationSchemaType } from 'shared-schemas';
 
 @Injectable()
 export class MetricsService {
